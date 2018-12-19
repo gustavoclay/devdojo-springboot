5086895004b717b625b9ea08525ae07a1ed20f60
Gustavo Clay <gustavoclay@gmail.com>
2018-12-19 17:08:09 -0200

    Aula 02 - Setup do projeto. Continuar a partir da aula 03.

--- src/main/java/br/com/devdojo/awesome/endpoint/StudentEndPoint.java
+++ src/main/java/br/com/devdojo/awesome/endpoint/StudentEndPoint.java
@@ -1,14 +1,21 @@
 package br.com.devdojo.awesome.endpoint;
 
+import br.com.devdojo.awesome.model.Student;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static java.util.Arrays.asList;
 
 @RestController
 @RequestMapping("student")
 public class StudentEndPoint {
 
-
-
-
+	@RequestMapping(method = RequestMethod.GET, path = "/list")
+	public List<Student> listAll() {
+		return asList(new Student("Teste"), new Student("Teste1"));
+	}
 
 }
